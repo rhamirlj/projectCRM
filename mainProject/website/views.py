60c9e690c62461d1725074b5ac87389886ef98db
rhamirl <rhamirlj5@gmail.com>
2023-03-15 11:42:03 +0800

    login and logout
     Function Done

--- mainProject/website/views.py
+++ mainProject/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -8,10 +8,21 @@ def home(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-    
-    return render(request,'home.html',{})
+        #authenticate
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "you have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
 
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "you have been Logged Out...")
+    return redirect('home')
