33ae484c77fb81f35a550264524a3429faf19d39
rhamirl <rhamirlj5@gmail.com>
2023-03-15 10:40:38 +0800

    ongoing

--- mainProject/website/views.py
+++ mainProject/website/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    #check to see if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+    
     return render(request,'home.html',{})
+
+
+
+def logout_user(request):
+    pass
